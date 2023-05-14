dfaf67e6f7a862604e6c43239923af27721aad4f
anji907 <anji.sk129@gmail.com>
2023-05-14 13:42:52 +0900

    ユーザーの新規登録、削除機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+require 'debug'
+
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(params[:user][:email], params[:user][:password])
+      redirect_to root_path, notice: 'User created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    redirect_to root_path, notice: 'User destroyed', status: :see_other
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+    end
+end
