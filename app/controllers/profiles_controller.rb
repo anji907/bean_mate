c3cd389327c00cc216632fec19f53c69d33528b5
anji907 <anji.sk129@gmail.com>
2023-05-15 15:07:19 +0900

    プロフィール編集機能をざっくり実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,9 +5,21 @@ class ProfilesController < ApplicationController
 
   def edit; end
 
+  def update
+    if @user.update(profile_params)
+      redirect_to profiles_path, notice: 'Profile was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
     def set_user
       @user = current_user
+    end
+
+    def profile_params
+      params.require(:user).permit(:nickname, :bio, :sns_identifier, avatars: [])
     end
 end
