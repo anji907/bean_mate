8d5d455cb9a7498df3247ad29df70d393c18c369
anji907 <anji.sk129@gmail.com>
2023-05-17 10:39:53 +0900

    メールの送信を許可

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
