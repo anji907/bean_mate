348120504dc367582b5e48ce304466024487994d
anji907 <anji.sk129@gmail.com>
2023-05-20 17:30:06 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[create destroy]
   resource :profiles, only: %i[show edit update]
   resources :password_resets, only: %i[new create edit update]
+  resources :cafes, only: :index
 end
