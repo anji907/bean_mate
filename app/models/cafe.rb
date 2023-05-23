8462f52c71fbf42d3f4e783e61329fb6353ceed0
anji907 <anji.sk129@gmail.com>
2023-05-23 11:05:12 +0900

    ransackで検索できる属性を設定

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,3 +1,11 @@
 class Cafe < ApplicationRecord
   has_many_attached :images
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "created_at", "description", "id", "latitude", "longitude", "name", "place_id", "updated_at", "website", "weekday_text"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["images_attachments", "images_blobs"]
+  end
 end
