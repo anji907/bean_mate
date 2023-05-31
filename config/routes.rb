3b9f010a8413a438b41a8548103399173091d6ce
anji907 <anji.sk129@gmail.com>
2023-05-31 16:08:25 +0900

    ActionCableのルーティング等設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   get '/signup', to: 'users#new'
   get '/login', to: 'user_sessions#new'
