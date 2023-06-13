e33fa45766ccb68223dce58bf4c8b4ce814787d8
anji907 <anji.sk129@gmail.com>
2023-06-13 14:49:06 +0900

    line_user_idをもとにユーザーを取得

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,10 +1,10 @@
 class LineBotController < ApplicationController
-  # skip_before_action :require_login
+  skip_before_action :require_login
   protect_from_forgery with: :null_session, only: [:callback]
 
   def callback
-    current_user.update(line_user_id: params['events'][0]['source']['userId'])
-    push_line(current_user.line_user_id)
+    user = User.find_by(line_user_id: params['events'][0]['source']['userId'])
+    push_line(user.line_user_id)
     head :ok
   end
 end
