faba46334bc23459f4b8c0d5700f7694522b7882
anji907 <anji.sk129@gmail.com>
2023-06-13 13:40:36 +0900

    ステータスコード200を返すように変更

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,6 +1,8 @@
 class LineBotController < ApplicationController
+  skip_before_action :require_login
   protect_from_forgery with: :null_session, only: [:callback]
 
   def callback
+    head :ok
   end
 end
