30d18c2cd4918c4bf15f81c6401b7550169b3a53
anji907 <anji.sk129@gmail.com>
2023-06-13 14:37:05 +0900

    skip_before_actionを解除

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,5 +1,5 @@
 class LineBotController < ApplicationController
-  skip_before_action :require_login
+  # skip_before_action :require_login
   protect_from_forgery with: :null_session, only: [:callback]
 
   def callback
