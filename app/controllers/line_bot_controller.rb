961f66bc3e46cee0b639e90a880be60e12f1acd0
anji907 <anji.sk129@gmail.com>
2023-06-14 15:15:31 +0900

    一度空にする

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,12 +1,7 @@
-require 'lib/line_messaging_api/push_message'
-
 class LineBotController < ApplicationController
   skip_before_action :require_login
   protect_from_forgery with: :null_session, only: [:callback]
 
   def callback
-    user = User.find_by(line_user_id: params['events'][0]['source']['userId'])
-    push_line(user.line_user_id)
-    head :ok
   end
 end
