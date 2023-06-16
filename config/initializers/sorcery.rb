f48af280f8c498f6059d2f81af242f9ebc41d65d
anji907 <anji.sk129@gmail.com>
2023-06-16 13:55:59 +0900

    scopeの指定の仕方を変更

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -222,7 +222,7 @@ Rails.application.config.sorcery.configure do |config|
   config.line.key = ENV['LINE_LOGIN_CHANNEL_ID']
   config.line.secret = ENV['LINE_LOGIN_CHANNEL_SECRET']
   config.line.callback_url = "https://bean-mate.herokuapp.com/oauth/callback?provider=line"
-  config.line.scope = "profile%20openid"
+  config.line.scope = "profile openid"
   # config.line.bot_prompt = "normal"
   # config.line.user_info_mapping = {name: 'displayName'}
 
