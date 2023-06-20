bd27f6924c19f8c9ad8f866c8976afad761cfa7a
anji907 <anji.sk129@gmail.com>
2023-06-20 14:17:08 +0900

    flashのキーを変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_to root_path, flash: { success: 'Login successful' }
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
@@ -16,6 +16,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, notice: 'Logged out!', status: :see_other
+    flash[:info] = 'Logged out!'
+    redirect_to login_path, status: :see_other
   end
 end
