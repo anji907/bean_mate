a39e9098110d57e6e8d8b814f09b3ae513d883d2
anji907 <anji.sk129@gmail.com>
2023-06-21 13:55:54 +0900

    外部認証かどうかのロジックを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :external_auth
+  # attr_accessor :external_auth
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] && !external_auth }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] && !external_auth }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] && !external_auth }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] && !external?}
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] && !external? }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] && !external? }
 
   validates :nickname, presence: true
   validates :email, uniqueness: true
