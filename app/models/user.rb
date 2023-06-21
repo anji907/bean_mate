214234860bf7095d67df65faa5a145a1de3704c8
anji907 <anji.sk129@gmail.com>
2023-06-21 13:35:34 +0900

    外部認証かどうかを判断する仮想属性を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  attr_accessor :external_auth
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] && !external_auth }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] && !external_auth }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] && !external_auth }
 
   validates :nickname, presence: true
   validates :email, uniqueness: true
