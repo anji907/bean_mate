bab091ea027f89ced8cc66c7fe4c8f88f96c12d9
anji907 <anji.sk129@gmail.com>
2023-06-28 08:24:14 +0900

    読み込みを削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,4 +9,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 # pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
-pin_all_from "app/javascript/channels", under: "channels"
+# pin_all_from "app/javascript/channels", under: "channels"
