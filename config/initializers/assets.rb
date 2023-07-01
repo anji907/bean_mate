1f177a0825b9811cff2cec4f9b083114b574065e
anji907 <anji.sk129@gmail.com>
2023-07-01 15:03:28 +0900

    プリコンパイル対象をもとに戻した

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,9 +5,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font", "app/javascript")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( custom.css, javascript/application.js )
+Rails.application.config.assets.precompile += %w( custom.css )
