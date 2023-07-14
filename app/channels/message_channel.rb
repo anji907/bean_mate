fe6e9af9da4eed93d950e10436d59dfde09179d3
anji907 <anji.sk129@gmail.com>
2023-07-14 16:17:56 +0900

    messageインスタンスを保存する処理をチャネルファイルに移動

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -6,4 +6,8 @@ class MessageChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def speak(data)
+    Message.create!(content: data['message'], user_id: current_user.id, room_id: data['room_id'])
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,2 @@
 class MessagesController < ApplicationController
-
-  def create
-    @message = Message.new(message_params)
-    if @message.save
-      ActionCable.server.broadcast 'message_channel', { message: render_message(@message) }
-      @message.create_message_notification!(current_user)
-      head :ok
-    else
-      redirect_to room_path(@message.room)
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content, :room_id).merge(user_id: current_user.id)
-  end
-
-  def render_message(message)
-    ApplicationController.renderer.render partial: 'messages/message', locals: { message: message }
-  end
 end
