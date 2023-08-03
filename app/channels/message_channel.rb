d0f2930cfaceb3ba18c9ac161e63d2b0ba3be8fc
anji907 <anji.sk129@gmail.com>
2023-08-03 11:58:58 +0900

    個別のコネクションを確立できるように設定

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,13 +1,19 @@
 class MessageChannel < ApplicationCable::Channel
+  include Rails.application.routes.url_helpers
+
   def subscribed
-    stream_from "message_channel"
+    @room = Room.find(params[:room_id])
+    stream_for @room
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 
   def speak(data)
-    Message.create!(content: data['message'], user_id: current_user.id, room_id: data['room_id'])
+    message = Message.create!(content: data['message'], user_id: current_user.id, room_id: data['room_id'])
+    self.class.broadcast_to(@room, { message: message.content, user: message.user.nickname,
+      user_id: message.user.id, avatar_url: rails_blob_url(message.user.avatars[0]) })
   end
 end
